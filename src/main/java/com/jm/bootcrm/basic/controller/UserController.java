8b486b72ad71a4e3a872f5ca17608ebb5ee93677
Johnny Miller <johnnysviva@outlook.com>
2018-08-17 17:58:20 +0800

    💡Update JDBC's driver: com.mysql.cj.jdbc.Driver (recommended by MySQL)

--- src/main/java/com/jm/bootcrm/basic/controller/UserController.java
+++ src/main/java/com/jm/bootcrm/basic/controller/UserController.java
@@ -2,6 +2,8 @@ package com.jm.bootcrm.basic.controller;
 
 import com.jm.bootcrm.basic.pojo.User;
 import com.jm.bootcrm.basic.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,6 +25,7 @@ import java.io.PrintWriter;
 @Controller
 @RequestMapping("/userController")
 public class UserController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private UserService userService;
 
@@ -31,15 +34,26 @@ public class UserController {
         PrintWriter out = response.getWriter();
         out.write("Hello!");
     }
+
     @RequestMapping("/login")
     public ModelAndView login(User user) {
         ModelAndView modelAndView = new ModelAndView();
         String loginResult = userService.verifyLogin(user);
         if ("success".equals(loginResult)) {
             modelAndView.setViewName("/views/userList.jsp");
-        }else{
+        } else {
             modelAndView.setViewName("/views/index00000.jsp");
         }
+        logger.error("This is a ERROR message");
+        logger.warn("This is a WARN message");
+        logger.info("This is a INFO message");
+        logger.debug("This is a DEBUG message");
+        logger.trace("This is a INFO message");
+        System.out.println("isErrorEnabled: "+logger.isErrorEnabled());
+        System.out.println("isDebugEnabled: "+logger.isDebugEnabled());
+        System.out.println("isErrorEnabled: "+logger.isWarnEnabled());
+        System.out.println("isInfoEnabled: "+logger.isInfoEnabled());
+        System.out.println("isTraceEnabled: "+logger.isTraceEnabled());
         return modelAndView;
     }
 }
