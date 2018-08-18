4cac0bc0f78e0df0123ff7ca032988d22d80b3ed
Johnny Miller <johnnysviva@outlook.com>
2018-08-18 11:58:08 +0800

    🚮Delete JavaScript console log

--- src/main/webapp/static/common/path.js
+++ src/main/webapp/static/common/path.js
@@ -2,5 +2,3 @@
 var curPath = window.document.location.href;
 var pathName = window.document.location.pathname;
 var basePath = curPath.substring(0, curPath.indexOf(pathName)) + "/bootcrm";
-
-console.log("basePath" + basePath);
